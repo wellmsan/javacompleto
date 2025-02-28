7108d485a79d4b09304f797eb9360be0a1eff6a7
WELBER <4468699-wellmsan@users.noreply.gitlab.com>
2025-02-28 12:23:35 -0300

    Fix nome do arquivo

---
+++ principiosjava/src/PrincipiosJava.java
@@ -0,0 +1,16 @@
+/*
+    Está é uma classe em Java.
+    Seu nome deve ser o mesmo nome do arquivo.java
+    Lembre sempre de uar o nome com iniciais maiúsculas, inclusive em nomes compostos
+ */
+public class PrincipiosJava {
+
+    // Este é o método principal da classe Java (main)
+    public static void main(String[] args) {
+        int numeroUm = 2; // Primeira variável inteira
+        int numeroDois = 2; // Segunda variável inteira
+        int soma = numeroUm + numeroDois; //Variável que soma as duas variáveis anteriores
+
+        System.out.println("Resultado: " + soma); // Imprime o resultado no console
+    }
+}
