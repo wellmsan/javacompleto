b6af619e69ff33e10cddc6860e2529aa44897ce0
WELBER <4468699-wellmsan@users.noreply.gitlab.com>
2025-02-28 11:27:59 -0300

    Add HelloWorld de exemplo em java

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Olá mundo!");
+    }
+}
+
+/*
+ * Classe principal do programa
+ * a ser construído
+ */
+// public class HelloWorld { // Nome da classe
+// // Método principal
+// public static void main(String args[]) {
+// System.out.println("Olá mundo!");
+// }
+// }
