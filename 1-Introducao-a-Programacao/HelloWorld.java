8600eef88752ceacf21625505e15f09e7067f663
WELBER <welbermsantana@gmail.com>
2025-02-28 13:37:20 -0300

    Update HelloWorld.java

--- 1-Introducao-a-Programacao/HelloWorld.java
+++ 1-Introducao-a-Programacao/HelloWorld.java
@@ -4,14 +4,3 @@ public class HelloWorld {
         System.out.println("Olá mundo!");
     }
 }
-
-/*
- * Classe principal do programa
- * a ser construído
- */
-// public class HelloWorld { // Nome da classe
-// // Método principal
-// public static void main(String args[]) {
-// System.out.println("Olá mundo!");
-// }
-// }
