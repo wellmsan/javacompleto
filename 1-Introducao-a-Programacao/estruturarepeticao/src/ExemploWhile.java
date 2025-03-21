f43652e914f3b90abc1ad281988afa6aa2648231
WELBER <4468699-wellmsan@users.noreply.gitlab.com>
2025-03-21 20:20:22 -0300

    Exemplo de repetição while

---
+++ 1-Introducao-a-Programacao/estruturarepeticao/src/ExemploWhile.java
@@ -0,0 +1,14 @@
+public class ExemploWhile {
+    public static void main(String[] args) {
+        int numero = 1;
+
+//        while(numero <= 10){
+//            System.out.println(numero++);
+//        }
+
+        do{
+            System.out.println(numero++);
+        }while (numero <= 100);
+
+    }
+}
