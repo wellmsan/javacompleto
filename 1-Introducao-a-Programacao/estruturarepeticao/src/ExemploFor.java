32ef66778f91cb5a351e6e1d630a5205bbd7e758
WELBER <4468699-wellmsan@users.noreply.gitlab.com>
2025-03-21 20:19:55 -0300

    Exemplo de repetição for

---
+++ 1-Introducao-a-Programacao/estruturarepeticao/src/ExemploFor.java
@@ -0,0 +1,13 @@
+public class ExemploFor {
+    public static void main(String[] args){
+        int numero = 1;
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(numero++);
+        }
+
+        System.out.println(numero);
+
+
+    }
+}
