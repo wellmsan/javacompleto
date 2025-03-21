69e43442b1cd59ea21c7e33d4537f2197cd300ba
WELBER <4468699-wellmsan@users.noreply.gitlab.com>
2025-03-20 21:39:22 -0300

    Exercício Categoria de Atleta

---
+++ 1-Introducao-a-Programacao/estruturacontrole/src/CategoriaAtleta.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CategoriaAtleta {
+    public static void main(String[] args) {
+        int idade;
+        String categoria;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite sua idade: ");
+        idade = entrada.nextInt();
+
+        if(idade < 5)
+            categoria = "Idade inválida para competir";
+         else if(idade > 5 && idade <= 10)
+            categoria = "Infantil";
+         else if(idade >= 11 && idade <= 15)
+            categoria = "Juvenil";
+         else if(idade >= 16 && idade <= 19)
+            categoria = "Júnior";
+         else if(idade >= 20 && idade <= 40)
+            categoria = "Adulto";
+         else
+            categoria = "Master";
+
+        System.out.println(categoria);
+    }
+}
