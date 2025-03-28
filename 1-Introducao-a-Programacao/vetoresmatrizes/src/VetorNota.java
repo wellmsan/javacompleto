2b0cce3b8e0147a5acb201de7ebc3d42e61ac71b
WELBER <4468699-wellmsan@users.noreply.gitlab.com>
2025-03-28 20:01:05 -0300

    add vetor exemplo

---
+++ 1-Introducao-a-Programacao/vetoresmatrizes/src/VetorNota.java
@@ -0,0 +1,21 @@
+public class VetorNota {
+    public static void main(String[] args) {
+        int[] notas = new int[5];
+        notas[0] = 85;
+        notas[1] = 90;
+        notas[2] = 25;
+        notas[4] = 12;
+
+        for(int nota : notas){
+            System.out.println("Nota: " + nota);
+        }
+
+
+
+
+
+//        for (int nota : notas) {
+//            System.out.println("Nota: " + nota);
+//        }
+    }
+}
---
+++ 1-Introducao-a-Programacao/vetoresmatrizes/src/VetorNumero.java
@@ -0,0 +1,11 @@
+public class VetorNumero {
+    public static void main(String[] args) {
+        int[] numeros = {10, 20, 30, 40, 50};
+        System.out.println("Primeiro número: " + numeros[0]);
+
+        for(int i = 0; i < numeros.length; i++){
+            System.out.println("Posição " + i + " " + numeros[i]);
+        }
+
+    }
+}
