8792d5d4ba9e509c21fa0877cc2a3e3df7a4bf10
WELBER <4468699-wellmsan@users.noreply.gitlab.com>
2025-04-10 23:21:06 -0300

    exemplo arraylist

--- 2-Estrutura-de-Dados-e-Modularizacao/1-estruturasdados/src/ExemploArrayList.java
+++ 2-Estrutura-de-Dados-e-Modularizacao/1-estruturasdados/src/ExemploArrayList.java
@@ -3,16 +3,32 @@ import java.util.ArrayList;
 public class ExemploArrayList {
     public static void main(String[] args) {
         ArrayList<String> lista = new ArrayList<>();
+
         lista.add("Java");
         lista.add("Python");
         lista.add("C++");
+        lista.add(1, "JavaScript"); // Insere na posição 1
 
-        System.out.println("Lista: " + lista);
-        System.out.println("Get (1): " + lista.get(1));
-        lista.remove("Python");
-        System.out.println("Lista após remoção: " + lista);
-        lista.add("PHP");
-        System.out.println("Get (1): " + lista.get(1));
-        System.out.println("Lista após inserção: " + lista);
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println("Elemento: " + lista.get(i));
+        }
+
+        System.out.println("A lista contém Java? " + lista.contains("Java"));
+
+
+        System.out.println("Índice de C++: " + lista.indexOf("C++"));
+
+
+        System.out.println("Tamanho da lista: " + lista.size());
+
+
+        lista.clear();
+        System.out.println("Lista após clear: " + lista);
+
+
+
+        System.out.println(lista);
     }
 }
+
+
